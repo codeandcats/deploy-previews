39d3a61a1277f9944393d7eb8683f70960deb600
Benji <codeandcats@gmail.com>
2020-11-13 11:43:34 +1100

    feat: cycle background color

--- website/src/index.ts
+++ website/src/index.ts
@@ -1,1 +1,12 @@
-document.getElementById("root").innerText = "⚡️ The greatest website ⚡️";
+(function () {
+  document.getElementById("root").innerText = "⚡️ The greatest website ⚡️";
+  
+  let bgHue = 0;
+  
+  function updateBg() {
+    bgHue = (bgHue + 1) % 360;
+    document.body.style.backgroundColor = `hsl(${bgHue} 70% 40%)`;
+  }
+
+  setInterval(updateBg, 25);
+})();
